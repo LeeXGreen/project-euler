9409f5ac73bc4bcc03278e19bd654358e78873cd
Lee Green <lee.x.green@gmail.com>
2013-10-21 01:20:34 -0400

    new 003 -- still lame, collab edited w/Matt

--- solutions/003.rb
+++ solutions/003.rb
@@ -3,16 +3,21 @@
 require 'prime'
 
 class Problem003
-    def self.run
-        num = 600851475143
+  def self.run
+    num = 600851475143
+    # for each prime, repeatedly divide out until it divides no more
 
-        # find all primes smaller than sqrt(num)
-        last = Math.sqrt(num).floor
-        lp = Prime.take_while { |p| p < last }
-
-        # find the largest of these that is a factor
-        lp.reverse_each { |p| return p if num % p == 0 }
+    Prime.each(num) do |p|
+      while(num % p == 0)
+        num = num / p
+      end
+      
+      # when the number left is prime, this is the answer
+      return num if Prime.prime?(num)
     end
+
+    raise "Logic Error" # should never get here, if we understand math
+  end
 end
 
 p Problem003.run
