1550422ff8fbb50e06af7a75ba6602db9a204a85
Lee Green <lee.x.green@gmail.com>
2013-10-23 09:30:30 -0400

    Comments

--- solutions/015.rb
+++ solutions/015.rb
@@ -1,10 +1,15 @@
 #!/usr/bin/env ruby
 #
+
 class Problem015
+    # could memoize this for better performance
     def self.fact(i)
         (1..i).reduce(:*) || 1
     end
 
+    # Since we must always move from upper left to lower right,
+    # we will always have the same amount of [right, down].
+    # Therefore, this is just a combinatorics problem: (right+down) choose (right)
     def self.run
         x_size = 20
         y_size = 20
